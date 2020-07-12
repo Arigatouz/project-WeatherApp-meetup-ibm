bfe72eb741cb2a48483284ecd06193d0802836ac
Ali Gamal Ali <hictors@gmail.com>
2020-07-12 21:21:17 +0200

    fixing  according to the instructions

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const endPointData = [],
+const projectData = {},
   cors = require("cors"),
   express = require("express"),
   app = express(),
@@ -9,9 +9,23 @@ app.use(express.static("website"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/all", sendData);
+function sendData(req, res) {
+  res.send(projectData);
+  console.log(projectData);
+
+}
+
+app.post("/addWeather", storeWeatherData);
+
+function storeWeatherData(req, res) {
+  projectData.date = req.body.date;
+  projectData.temp = req.body.temperature;
+  projectData.feelings = req.body.feelings;
+  console.log("data received");
+  res.send(projectData);
+  console.log(projectData);
+}
 app.listen(PORT, () => {
   console.log(`SERVER IS RUNNING ON PORT: ${PORT}`);
 });
--- website/app.js
+++ website/app.js
@@ -1,11 +1,15 @@
+// initializing Date
+const date = new Date();
+const newDate = `${
+  date.getMonth() + 1
+}/${date.getDate()}/${date.getFullYear()}`;
 // working APi http://api.openweathermap.org/data/2.5/weather?q=egypt&units=metric&APPID=01ad071dfa318cd13176b18744267ba6&lang=en//
 const keyAPI = "01ad071dfa318cd13176b18744267ba6";
 const APILink =
-  "http://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=";
-const Metric = "&units=metric&";
-const weatherList = document.querySelector(".weather__list");
-const getFetch = async (method, name, data) => {
-  const result = await fetch(`${APILink}${name}${Metric}APPID=${keyAPI}`, {
+  "http://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=";
+const feelings = document.getElementById("feelings");
+const getFetch = async (method, zip, data) => {
+  const result = await fetch(`${APILink}${zip}&APPID=${keyAPI}`, {
     method: method,
     body: JSON.stringify(data),
     headers: {
@@ -20,7 +24,7 @@ const getFetch = async (method, name, data) => {
       const newData = await result.json();
       return newData;
     } else {
-      return response.json().then((err) => {
+      return result.json().then((err) => {
         console.log(err);
         throw new Error("something went Wrong ");
       });
@@ -30,48 +34,59 @@ const getFetch = async (method, name, data) => {
     throw new Error("Request Can't be Completed");
   }
 };
-
-const getWeather = async (name) => {
-  const city = document.getElementById("city").value;
-  name = city;
-  await getFetch("get", name).then((newData) => {
-    updateUI(newData);
+const postData = async (url, data) => {
+  const resultPosted = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
   });
+
+  try {
+    const newData = await resultPosted.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+const getWeather = async (zip) => {
+  const cityZip = document.getElementById("zipCode").value;
+  zip = cityZip;
+  await getFetch("get", zip)
+    .then((newData) => {
+      postData("/addWeather", {
+        temperature: newData.main.temp,
+        date: newDate,
+        feelings: document.getElementById("content").value,
+      });
+      console.log(newData.main.temp);
+      console.log(newDate);
+      console.log(document.getElementById("feelings").value);
+    })
+    .then(() => {
+      updateUI("/all");
+    });
 };
 
 const formButton = document.getElementById("form");
 formButton.addEventListener("submit", (e) => {
   e.preventDefault();
   getWeather();
-  document.getElementById("city").value = "";
+  document.getElementById("zipCode").value = "";
 });
 
-const updateUI = (passedInformation) => {
-  const postTemplate = document.getElementById("post-template");
-  const postEl = document.importNode(postTemplate.content, true);
-  postEl.querySelector(
-    "h3"
-  ).textContent = `This Weather is For ${passedInformation.name}`;
-  postEl.querySelector(
-    "span"
-  ).textContent = `Weather Clouds :  ${passedInformation.weather[0].main} `;
-  postEl.querySelector(
-    ".temp"
-  ).textContent = `Temperature is ${passedInformation.main.temp} celsius `;
-  postEl.querySelector(
-    ".min__temp"
-  ).textContent = `Min temperature is ${passedInformation.main["temp_min"]} celsius `;
-  postEl.querySelector(
-    ".max__temp"
-  ).textContent = `Max temperature is ${passedInformation.main["temp_max"]} celsius `;
-  postEl.querySelector(
-    ".pressure"
-  ).textContent = `pressure is ${passedInformation.main.pressure} pascal `;
-  postEl.querySelector(
-    ".wind__speed"
-  ).textContent = `wind speed is ${passedInformation.wind.speed} knots `;
-  postEl.querySelector(
-    ".wind__degree"
-  ).textContent = `wind direction is ${passedInformation.wind.deg} degrees `;
-  weatherList.appendChild(postEl);
+const updateUI = async (url) => {
+  const request = await fetch(url);
+  try {
+    const allData = await request.json();
+    console.log(allData);
+    document.getElementById("date").innerHTML = allData.date;
+    document.getElementById("temp").innerHTML = allData.temp;
+    document.getElementById("content").innerHTML = allData.feelings;
+  } catch (error) {
+    console.log("error", error);
+    throw Error("can't get data ");
+  }
 };
